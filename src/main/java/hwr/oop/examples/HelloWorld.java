f5cc16be8a9f5464cf555aea38aa45fb1f483873
Tom Sonntag <tom.sonntag@sap.com>
2022-04-11 09:43:10 +0200

    Add: New function-testing

--- src/main/java/hwr/oop/examples/HelloWorld.java
+++ src/main/java/hwr/oop/examples/HelloWorld.java
@@ -1,7 +1,13 @@
 package hwr.oop.examples;
 
 public class HelloWorld {
+    String name;
+
+    public HelloWorld(String name) {
+        this.name = name;
+    }
+
     String getOutputString() {
-        return "Hello World";
+        return "Hello " + name;
     }
 }
--- src/test/java/hwr/oop/examples/HelloWorldTest.java
+++ src/test/java/hwr/oop/examples/HelloWorldTest.java
@@ -4,11 +4,18 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class HelloWorldTest {
+
     @Test
     void helloWorld_givesUsAHelloWorldString() {
-        HelloWorld helloWorld = new HelloWorld();
+        HelloWorld helloWorld = new HelloWorld("World");
         String output = helloWorld.getOutputString();
         Assertions.assertThat(output).isEqualTo("Hello World");
     }
 
+    @Test
+    void helloWorld_givesAProvidedName() {
+        HelloWorld helloWorld = new HelloWorld("Tom");
+        String output = helloWorld.getOutputString();
+        Assertions.assertThat(output).isEqualTo("Hello Tom");
+    }
 }
